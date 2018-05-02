93840da5ad0a9a64131381784a95d9527355afec
Olof Engström <oeng@kth.se>
2018-05-02 16:18:38 +0200

    Add skeleton code

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,6 +1,28 @@
 def test():
     print("Hello, KTH")
 
+class Analyzer():
+    # Constructor
+    def __init__(self):
+        pass
+
+    """
+    TODO: Yield source code files one at a time (gustaf fixar)
+    """
+    def get_files_generator(self, repo_path):
+        pass
+
+    """
+    TODO: Parse function and method names from a java file
+    """
+    def parse_function_names(self):
+        pass
+    
+    """
+    TODO: Parse class names from a java file
+    """
+    def parse_class_names(self):
+        pass
 
 if __name__ == '__main__':
     test()
