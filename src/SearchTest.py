78c958c3b5ec135cd284a499dc3196abe79f1464
Gustaf <gustaf.w.e@gmail.com>
2018-05-05 23:21:52 +0200

    Reimplemented bulk indexing to use elasticsearch python helper instead

--- src/SearchTest.py
+++ src/SearchTest.py
@@ -2,6 +2,8 @@ from elasticsearch import Elasticsearch
 
 
 def search_test():
+    # Nice examples if using Python: https://github.com/elastic/elasticsearch-py/blob/master/example/queries.py
+
     es = Elasticsearch()
     res = es.search(
         index='test',
